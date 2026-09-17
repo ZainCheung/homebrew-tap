cf29e3674af086c803760c35c288041dc378ab96
ZainCheung <ZainCheung@gmail.com>
2026-09-17 23:57:01 +0800

    fix: use public release repository homepage

--- Formula/ploy.rb
+++ Formula/ploy.rb
@@ -1,6 +1,6 @@
 class Ploy < Formula
   desc "First-party macOS runner for Ploy local iOS preview builds"
-  homepage "https://github.com/ZainCheung/ploy"
+  homepage "https://github.com/ZainCheung/ploy-cli"
   version "0.1.0"
   url "https://github.com/ZainCheung/ploy-cli/releases/download/v#{version}/ploy-agent-#{version}.tar.gz"
   sha256 "efde481fbaaa24ead70dbdb34e4e770f0d7342686851930b8c6d63a99490c4db"
